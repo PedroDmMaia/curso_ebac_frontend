14516326fcaf96a2a5907702a34916c82734f26a
Pedro Maia <pedrodelmonico@hotmail.com>
2023-10-10 22:17:56 -0300

    commit initial

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function Multiplica(valor1, valor2) {
+    return valor1 * valor2;
+}
+function Ola(nome) {
+    return `Olá ${nome}`;
+}
+console.log(Multiplica(10, 5));
+console.log(Ola('Pedro'));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function Multiplica(valor1: number, valor2: number): number {
+    return valor1 * valor2
+}
+
+function Ola(nome: string): string {
+    return `Olá ${nome}`
+}
+
+console.log(Multiplica(10, 5))
+console.log(Ola('Pedro'))
